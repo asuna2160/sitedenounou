bc8cc2de4e47187f865fd8b1d943f256a530f756
Celi28M <les.super.amis@gmail.com>
2018-06-17 05:22:52 +0200

    fix in case of session already started

--- traitementloginnounou.php
+++ traitementloginnounou.php
@@ -1,16 +1,18 @@
 <?php
 
-session_start();
-
 require_once 'database1.php';
-session_start();
- 
-$login = $_POST['login'];
-$passwd=$_POST['passwd'];
-$login_sth=$basedd->prepare("select count(*) from Nounou where login='$login' and motdepasse='$passwd'");
- 
-$login_sth->execute(array($login,$passwd));
-$tot=$login_sth->fetchAll();
+
+if(!isset($_SESSION))
+{
+	session_start();
+}
+
+	$login = $_POST['login'];
+	$passwd=$_POST['passwd'];
+	$login_sth=$basedd->prepare("select count(*) from Nounou where login='$login' and motdepasse='$passwd'");
+
+	$login_sth->execute(array($login,$passwd));
+	$tot=$login_sth->fetchAll();
 if($tot[0][0]==0)
 {
     echo "Erreurs sur login ou mot de passer";
